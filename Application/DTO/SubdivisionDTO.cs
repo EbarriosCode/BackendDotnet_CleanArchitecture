467379a05e67e748211e54eadb13d8c02a722021
EbarriosCode <guayoswing@gmail.com>
2021-03-08 00:07:46 -0600

    Creation of graphical interface in the Blazor SPA application, use of the backend and crud operations for Country and Subdivision models.

---
+++ Application/DTO/SubdivisionDTO.cs
@@ -0,0 +1,13 @@
+﻿using Countries.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO
+{
+    public class SubdivisionDTO : Subdivision
+    {
+    }
+}
---
+++ Countries.Common/Resources/Literals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Countries.Common.Resources
+{
+    public class Literals
+    {
+        public static string URI_API_COUNTRIES = "api/Countries";
+        public static string URI_API_SUBDIVISIONS = "api/Subdivisions";
+    }
+}
